93250c918758527b6af35d03518283e5049ab1f4
Angus Lo <szupie@gmail.com>
2015-08-24 19:57:10 -0400

    focus on title field by default on new item

--- js/directives/bkListView.js
+++ js/directives/bkListView.js
@@ -28,10 +28,12 @@ function bkListView() {
 		})
 
 		scope.$watch('Items.getCurrentList().items[0]', function() {
+			// on new item added
 			var newItem = element[0].querySelector('bk-item');
 			if (newItem) {
 				deselectAll();
 				newItem.classList.add('editable');
+				newItem.querySelector('.title input').focus(); // focus title field by default
 			}
 		});
 
