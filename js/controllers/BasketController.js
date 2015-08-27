2f8eb0b86ed90f18fd8b3ce73d099683d3bcf35c
Angus Lo <szupie@gmail.com>
2015-08-26 20:16:09 -0400

    tablet style

--- js/controllers/BasketController.js
+++ js/controllers/BasketController.js
@@ -2,6 +2,16 @@ angular
 	.module('app')
 	.controller('BasketController', BasketController);
 
-function BasketController() {
+function BasketController($mdSidenav) {
 	var vm = this;
+	vm.toggleListsView = toggleListsView;
+	vm.closeListsView = closeListsView;
+
+	function toggleListsView() {
+		$mdSidenav('left').toggle();
+	}
+
+	function closeListsView() {
+		$mdSidenav('left').close();
+	}
 }
